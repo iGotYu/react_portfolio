fa7546503d3e9642e66f683ab8418c81cfa57776
Andrew Yu <igotyuu421@gmail.com>
2021-05-25 23:01:04 -0700

    header/contact/aboutme

---
+++ src/components/contactMe/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./style.css"
+
+export default function ContactMe() {
+    return (
+        <div id="container3">
+        <div id="contact-me">
+          <h2>Contact Me</h2>
+        </div>
+        <div id="contact-me-links">
+          <ul>
+            <li>301-633-6775</li>
+            <li>igotyuu@gmail.com</li>
+            <li><a href="https://github.com/iGotYu">GitHub</a></li>
+            <li>
+              <a href="https://www.linkedin.com/in/igotyu/"
+                >LinkedIn</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+}
---
+++ src/components/header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./style.css"
+
+export default function Header() {
+    return (
+        <div id="header">
+            <h1>Andrew Yu's Portfolio</h1>
+      <nav className="navigation">
+        <ul>
+          <li><a href="#about-me">About Me</a></li>
+          <li><a href="#work">Work</a></li>
+          <li><a href="#contact-me">Contact Me</a></li>
+          <li><a href="#resume">Resume</a></li>
+        </ul>
+        </nav>
+        </div>
+    )
+}
