68af1398d39c9082c23f3399c73e81bc27a55896
Andrew Yu <igotyuu421@gmail.com>
2021-05-25 22:49:59 -0700

    initial commit

---
+++ src/components/aboutMe/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./style.css";
+
+export default function AboutMe() {
+  return (
+    <>
+      <div id="banner" />
+      <div id="container">
+        <div id="about-me">
+          <h2>About Me</h2>
+        </div>
+        <div id="about-me-info">
+          <p>
+            Hello! My name is Andrew Yu and I am a current student in the UW
+            bootcamp course. Prior to this program I was a Bartender for the Din
+            Tai Fung Restaurant company and a small time Twitch streamer (Link
+            below). I have been in customer service for nearly 10 years now. And
+            after so much time in this ive decided to make a career change with
+            this program.
+          </p>
+          <p>
+            It took some time and a bit of research for me to commit to this
+            program but after this first week I must say that the curriculum is
+            very well made and give us students so many tools to succeed! BEST
+            OF LUCK TO ALL AND THROUGH COLLABORATION WE SHALL ACHIEVE
+            GREATNESS!!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
